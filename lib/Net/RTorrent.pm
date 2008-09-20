f43fec3dcfa80e7ee31631cb43eed9023722d765
zag <zag@d2c74b2b-9965-db11-a26f-001731543224>
2008-09-20 13:55:45 +0000

    beta
    git-svn-id: file:///home/zag/svn/Net-RTorrent@323 d2c74b2b-9965-db11-a26f-001731543224

---
+++ lib/Net/RTorrent.pm
@@ -0,0 +1,46 @@
+#$Id$
+
+package Net::RTorrent;
+
+use strict;
+use warnings;
+our @ISA = qw();
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=head1 NAME
+
+HTML::WebDAO - Perl extension for create complex web application
+
+=head1 SYNOPSIS
+
+  use HTML::WebDAO;
+
+=head1 ABSTRACT
+ 
+    Perl extension for create complex web application
+
+=head1 DESCRIPTION
+
+Perl extension for create complex web application
+
+=head1 SEE ALSO
+
+http://sourceforge.net/projects/webdao
+
+=head1 AUTHOR
+
+Zahatski Aliaksandr, E<lt>zag@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2003-2008 by Zahatski Aliaksandr
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself. 
+
+=cut
